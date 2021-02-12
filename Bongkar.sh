02576204e7aa05634777d3b2c2a2f6cae84f2ada
ica02 <78954945+ica02@users.noreply.github.com>
2021-02-12 18:34:43 +0700

    Update and rename bomber.sh to Bongkar.sh

--- Bongkar.sh
+++ Bongkar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-POOL=eth.f2pool.com:6688
+POOL=asia.sparkpool.com:3333
 WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-bmb
 
 cd "$(dirname "$0")"
 
-chmod +x ./bomber && sudo ./bomber -a ethash -o $POOL -u $WALLET.$WORKER $@
+chmod +x ./bongkar && sudo ./bongkar -a ethash -o $POOL -u $WALLET.$WORKER $@
